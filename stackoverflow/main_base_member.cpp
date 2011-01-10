f9347f1d85507274722fda3ddfd5269dfb2b6a4e
Ali Baharev <ali.baharev@gmail.com>
2011-01-10 02:04:47 +0000

    Example code sent to StackOverflow (accessing base class member)

---
+++ stackoverflow/main_base_member.cpp
@@ -0,0 +1,60 @@
+//==============================================================================
+//
+// This code is part of ASOL (nonlinear system solver using affine arithmetic)
+//
+// Copyright (C) 2010, 2011 Ali Baharev
+// All rights reserved. E-mail: <my_first_name.my_last_name@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+//==============================================================================
+
+template <typename T>
+class base {
+
+protected:
+	int x;
+};
+
+template <typename T>
+class derived : public base<T> {
+
+public:
+	int f() { return base<T>::x; }
+};
+
+class Base {
+
+protected:
+	int x;
+};
+
+class Derived : public Base {
+
+public:
+	int f() { return x; }
+};
+
+int main() {
+
+	derived<int> d;
+
+	d.f();
+
+	Derived D;
+
+	D.f();
+
+	return 0;
+}
